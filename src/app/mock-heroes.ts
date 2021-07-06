e26c028fd85ee19c4acf6b872fd0645728c535bc
Toni Powell <tonipow3ll@gmail.com>
2021-07-06 12:20:28 -0400

    step two, we have a list that displays stuff!

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 2, name: 'Dr Nice' },
+    { id: 3, name: 'Doctor Strange' },
+    { id: 4, name: 'Obi-Wan' },
+    { id: 5, name: 'Princess Leia' },
+    { id: 6, name: 'Spider Man' },
+    { id: 7, name: 'Iron Man' },
+    { id: 7, name: 'Black Panther' },
+    { id: 9, name: 'Bruce Banner' },
+    { id: 10, name: 'Captain America' },
+    { id: 11, name: 'Thor' },
+]
